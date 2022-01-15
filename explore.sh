d3ffa8af76f709eecc5eb09572298a3a14425211
Jeffo007 <jefferson.sanchez@est.ikiam.edu.ec>
2022-01-15 14:34:23 -0500

    actualización x
    act

--- explore.sh
+++ explore.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# $1 is the file name
-# $2 is the column of interest
 
 echo "Nombre de la columna"
-cut -d ',' -f $2 $1 | head -n 1
+cut  -d  ','  -f $2 $1 | head -n 1
 echo "Nombre de distintos valores:"
-cut -d ',' -f $2 $1 | tail -n +2 | sort | uniq | wc -l
-echo "Valor minimo:"
+cut -d ',' -f $2 $1  |  tail -n +2 | sort | uniq | wc -l
+echo "valor  minimo:"
 cut -d ',' -f $2 $1 | tail -n +2 | sort -n | head -n 1
 echo "Valor Máximo:"
-cut -d ',' -f $2 $1 | tail -n +2 | sort -n | tail -n 1
+cut   -d ',' -f $2 $1  | tail -n +2 | sort -n | tail -n 1
